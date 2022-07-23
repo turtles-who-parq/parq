aad73f88918e38e3a97dc887b421952ff1fbe184
nia huang <nia.hq@yahoo.com>
2022-07-22 23:05:02 -0400

    testing

--- server/controllers/stripeController.js
+++ server/controllers/stripeController.js
@@ -4,25 +4,28 @@ const { Location } = require('../models/userModel');
 const stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
 const stripeController = async (req, res, next) => {
-  const { parking } = req.body;
-  console.log('parking==>', parking);
+  const parking = { priceInCents: 1507, day: 1, parkingName: 'Park#n' };
 
-  const parkingD = new Map([[1, parking]]);
+  const parkDummy = new Map([
+    [1, { priceInCents: 1500, day: 1, parkingName: 'Park1' }],
+    [2, { priceInCents: 2000, day: 3, parkingName: 'Park2' }],
+  ]);
 
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       mode: 'payment',
       line_items: parking.map(park => {
-        const storePark = parkingD.get(park.id);
+        const storePark = parkDummy.get(park.id);
         return {
           price_data: {
             currency: 'usd',
             product_data: {
-              name: storePark.hostUsername,
+              name: storePark.parkingName,
             },
-            unit_amount: storePark.price,
+            unit_amount: storePark.priceInCents,
           },
+          quantity: park.day,
         };
       }),
       success_url: `http://localhost:8080/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
