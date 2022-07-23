523de5aeb3d8aa5e64256f511563bf921e9f27fd
nia huang <nia.hq@yahoo.com>
2022-07-22 22:07:04 -0400

    testing

--- server/controllers/stripeController.js
+++ server/controllers/stripeController.js
@@ -1,30 +1,36 @@
-require('dotenv').config();
+//require('dotenv').config();
 //const { Location } = require('../models/userModel');
 
 //const stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
-const stripeController = (req, res, next) => {
-  console.log('reached stripoe controller');
-  console.log('req.body==>', req.body);
-  return next();
-  // const parkDummy = new Map([
-  //   [1, { price: 1500, day: 1 }],
-  //   [2, { price: 2000, day: 3 }],
-  // ]);
-
-  // try {
-  //   const session = await stripe.checkout.sessions.create({
-  //     payment_method_types: ['card'],
-  //     mode: 'payment',
-  //     line_items: parkDummy,
-  //     success_url: 'http://localhost:8080/',
-  //     cancel_url: 'http://www.google.com/',
-  //   });
-  //   res.locals.session = session;
-  //   return next();
-  // } catch (error) {
-  //   res.status(500).json({ error: error.message });
-  // }
+const stripeController = async (req, res, next) => {
+  try {
+    console.log('reached stripe controller');
+    console.log('req.body==>', req.body);
+    // const session = await stripe.checkout.sessions.create({
+    //   payment_method_types: ['card'],
+    //   mode: 'payment',
+    //   line_items: req.body.parking.map(park => {
+    //     const storePark = parkDummy.get(park.id);
+    //     return {
+    //       price_data: {
+    //         currency: 'usd',
+    //         product_data: {
+    //           name: storePark.parkingName,
+    //         },
+    //         unit_amount: storePark.priceInCents,
+    //       },
+    //       quantity: park.day,
+    //     };
+    //   }),
+    //   success_url: `http://localhost:8080/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
+    //   cancel_url: 'http://www.google.com/',
+    // });
+    // res.locals.session = session;
+    return next();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
 module.exports = stripeController;
